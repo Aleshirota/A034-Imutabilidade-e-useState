3e70b78248f66592b133ead35c89dbe9e4cb2073
Shiro <aleshirota@gmail.com>
2022-10-17 18:19:06 -0300

    Exercicios-a034-imutabilidade

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
+import React, { useState } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import PokemonCard from './Components/PokemonCard/PokemonCard';
 
 const GlobalStyles = createGlobalStyle`
@@ -14,15 +14,59 @@ const FlexContainer = styled.div`
   flex-wrap: wrap;
 `
 function App() {
-  const [pokemon, setPokemon] = useState({
+  const [pokemon0, setPokemon] = useState({
     name: "Pichu",
     type: "Electric",
     evolved: false,
     weight: 2,
     color: 'yellow',
-    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+    image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png',
     id: 0
   })
+
+  const [pokemon1, setPokemon1] = useState(
+    {
+      name: "Charmander",
+      type: "Fire",
+      evolved: false,
+      weight: 3,
+      color: 'orange',
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      id: 1
+    });
+    const [pokemon2, setPokemon2] = useState(
+      {
+        name: "Bulbassaur",
+        type: "grass",
+        evolved: false,
+        weight: 2,
+        color: 'green',
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        id: 2
+      });
+
+      const [pokemon3, setPokemon3] = useState(
+        {
+          name: "Squirtle",
+          type: "Water",
+          evolved: true,
+          weight: 3,
+          color: 'blue',
+          image: '  https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+          id: 3
+        });
+
+        const [pokemon4, setPokemon4] = useState(
+          {
+            name: "Pikachu",
+            type: "Electric",
+            evolved: true,
+            weight: 4,
+            color: 'yellow',
+            image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+            id: 4
+          });
+
 
   // Para fazer seus próximos pokemons, crie novos estados!
 
@@ -30,7 +74,11 @@ function App() {
   <GlobalStyles/>
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon0}  />
+      <PokemonCard pokemon={pokemon1}  />
+      <PokemonCard pokemon={pokemon2}  />
+      <PokemonCard pokemon={pokemon3}  />
+      <PokemonCard pokemon={pokemon4}  />
       {/* Crie aqui seus próximos pokemons! */}
     </FlexContainer>
   </>
